1b2a12b1ed30484d5c40ddbe6432da31d357f94d
Edvin Gavell <eddi@live.se>
2020-11-09 13:36:22 +0100

    added some more code

--- src/Lab2/BST.java
+++ src/Lab2/BST.java
@@ -13,62 +13,91 @@ public class BST { //BST -> Binära sökträd
         }
     }
 
-    public void remove(int n) {
+    public void remove(Node node, int n) {
         /*  2. Fortsätt med implementationen av ett binärt sökträd
             a) Lägg till metoder för att ta bort ett element, tänk på fallen:
             1. Noden har inga barn
             2. Noden har ett barn
             3. Noden har två barn
             Tänk också på att ni kan behöva hantera fallet där det som skall tas bort ligger i rot-noden. */
-        removeNode(this.root, n);
-    }
-
-    public Node removeNode(Node root, int x) {
-        if (root == null) {
-            return root;
-        }
-        if (x < root.key) {
-            root.left = removeNode(root.left, x);
-        } else if (x > root.key) {
-            root.right = removeNode(root.right, x);
-        } else {
-            if (root.left == null && root.right == null) {
-                return null;
-            } else if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
-                return root.left;
-            } else {
-                int min = minValue(root.right);
-                root.key = min;
-                root.right = removeNode(root.right,min);
+        if (node != null) { //As long as the node isnt null.
+            if (n == node.key) { //If nodekey is the same as we send in
+                if (node.left == null && node.right == null) { //The node doesnt have kids
+                    root = null;
+                } else if (node.left == null) { // The node only has one bigger child
+                    root = node.right;
+                } else if (node.right == null) { // The node only has one smaller child
+                    root = node.left;
+                } else { // The node has two childrens
+                    root = node.left;
+                    root.right = node.right;
+                }
+            } else if (n < node.key) { // our target to remove is smaller than current node.key
+                if (node.left!= null) {
+                    if (n == node.left.key) {
+                        if (node.left.left != null) {
+                            node.left = node.left.left;
+                        } else {
+                            node.left = null;
+                        }
+                    } else {
+                        remove(node.left, n);
+                    }
+                }
+            } else { // our target to remove is bigger than current node.key
+                if (node.right != null) {
+                    if (n == node.right.key) {
+                        if (node.right.right != null) {
+                            node.right = node.right.right;
+                        } else {
+                            node.right = null;
+                        }
+                    } else {
+                        remove(node.right, n);
+                    }
+                }
             }
         }
-        return root;
     }
 
-    public int minValue(Node node) {
-        if (node.left != null) {
-            return minValue(node.left);
-        } else {
-            return node.key;
+    public void remove(int n) {
+        remove(root, n);
+    }
+
+    /*
+            10 -> 15 -> 5 -> 12 -> 20 -> 7 -> 2 -> 6 -> 8
+                                      10
+                                   5          15
+                               2     7      12   20
+                                   6   8
+    */
+
+    public void traversePreOrder() { // NLR
+        // 5 -> 3 -> 1 -> 0 -> 2 -> 4 -> 8 -> 6 -> 7 -> 9 -> 10
+    }
+
+    public void traverseInOrder(Node tree) { // LNR
+        // 2 -> 5 -> 6 -> 7 -> 8 -> 10 -> 12 -> 15 -> 20
+        if (tree == null) {
+            return;
         }
+        traverseInOrder(tree.left);
+        System.out.println(tree.key);
+        traverseInOrder(tree.right);
     }
 
-    public void traversePreOrder() {
-
+    public void traverseReverseInOrder(Node tree) { // RNL
+        // 20 -> 15 -> 12 -> 10 -> 8 -> 7 -> 6 -> 5 -> 2
+        if (tree == null) {
+            return;
+        }
+        traverseReverseInOrder(tree.right);
+        System.out.println(tree.key);
+        traverseReverseInOrder(tree.left);
     }
 
-    public void traverseInOrder() {
-
-    }
-
-    public void traverseReverseInOrder() {
-
-    }
-
-    public void traversePostOrder() {
-
+    public void traversePostOrder() { // LRN
+        // 0 ->
     }
 
     private Node insertRec(Node tree, int key) {
@@ -113,34 +142,20 @@ public class BST { //BST -> Binära sökträd
         printInOrderRight(tree.left);
     }
 
-
     public static void main(String[] args) {
         BST bt = new BST();
-        //5, 6, 4, 7, 3, 8, 2, 9, 1, 10, 0
-        bt.insert(5);
-        bt.insert(6);
-        bt.insert(4);
-        bt.insert(7);
-        bt.insert(3);
-        bt.insert(8);
-        bt.insert(2);
-        bt.insert(9);
-        bt.insert(1);
         bt.insert(10);
-        bt.insert(0);
+        bt.insert(15);
+        bt.insert(5);
+        bt.insert(12);
+        bt.insert(20);
+        bt.insert(7);
+        bt.insert(2);
+        bt.insert(6);
+        bt.insert(8);
 
-        bt.printInOrderLeft();
+        //bt.traverseInOrder(bt.root);
+        //bt.traverseReverseInOrder(bt.root);
 
-        bt.remove(5);
-
-        bt.printInOrderLeft();
-
-        /*
-        System.out.println("Left to right: ");
-        bt.printInOrderLeft();
-        System.out.println();
-        System.out.println("Right to left: ");
-        bt.printInOrderRight();
-         */
     }
 }
